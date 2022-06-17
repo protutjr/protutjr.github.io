b4e086ae2ad6dd24e9d895074a088b399910bc05
Celso <celso@celso-desktop>
2022-06-16 23:49:34 -0300

    Aprimorando o sistema de dropdown

---
+++ js/dropdown.js
@@ -0,0 +1,20 @@
+function toggleDropdownState() {
+
+}
+
+function dropdownListener(evt) {
+    if (evt.target.tagName.toLowerCase() != "header") return;
+    
+    let parent = evt.target.parentElement;
+    if (parent.classList.contains("dropdown-visible"))
+        parent.classList.remove("dropdown-visible");
+    else
+        parent.classList.add("dropdown-visible");
+}
+
+window.addEventListener("load", () => {
+    let containers = document.getElementsByClassName("dropdown-container");
+    for (let container of containers) {
+        container.addEventListener("click", dropdownListener);
+    }
+});
